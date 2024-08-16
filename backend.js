02d9c90a288a731a1a0eef0cd59bfffa827f149b
yannickm260 <yannicmulemeri01@gmail.com>
2024-08-17 00:11:18 +0200

    Initial commit

--- backend.js
+++ backend.js
@@ -4,30 +4,30 @@ let inputField = document.getElementById("inputField");
 
 addToDoButton.onclick = function(){
     let paragraph;
-    // verifier si l'input n'est pas vide
+    // verify if the input is not empty
     if(inputField !=""){
-        paragraph = document.createElement('p') // creer un paragraphe
+        paragraph = document.createElement('p') // create a paragraph
     }
-    paragraph.innerText = inputField.value ; //valoriser ce paragraphe avec le contenu de l'input
+    paragraph.innerText = inputField.value ; //Fill the paragraph with the input's content
     
-    //styliser le paragraphe
+    // Style the paragraph
     paragraph.classList.add('bg-blue-400','text-black','rounded-lg','pl-2','py-3','px-2','m-2','w-full','relative','-left-2','break-all','cursor-pointer','last:mb-0','animate-anime')
 
 
 
-    toDoContainer.appendChild(paragraph) // inserer le paragraphe dans l'element toDoContainer
+    toDoContainer.appendChild(paragraph) // insert the paragraph into the toDoContainer element
 
-    // vider l'input quand le paragraphe est ajoutE
+      // Clear the input field after adding the task
     inputField.value = ""
 
-    // barrer le paragraphe apres avoir clique dessus
+     // Mark task as completed on click
     
     paragraph.addEventListener('click',function(){
         paragraph.classList.remove('bg-blue-400')
         paragraph.classList.add('bg-red-400','line-through')
     })
 
-    // supprimer le paragraphe
+    // Delete task on double click
 
     paragraph.addEventListener('dblclick',function(){
         toDoContainer.removeChild(paragraph);
